aa02bd0593b0c6bb6806264843c85b3d8cc09a91
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2024-10-21 16:49:45 -0300

    start

---
+++ src/DirectoryRouter.php
@@ -0,0 +1,14 @@
+<?php
+namespace Kingkernel;
+
+class DirectoryRouter 
+{
+    public function __construct()
+    {
+        
+    }
+    public function select()
+    {
+        return '';
+    }
+}
---
+++ src/MarraApi.php
@@ -0,0 +1,10 @@
+<?php
+namespace Kingkernel;
+
+class MarraApi 
+{
+    public function __construct()
+    {
+        
+    }
+}
